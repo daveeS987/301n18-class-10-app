19e1b7fb497121415acdce341eae62f34900a9ce
Davee Sok <daveesok@gmail.com>
2020-06-30 21:19:44 -0700

    added psql

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ require('dotenv').config;
 // Initialize the App
 const app = express();
 app.use(cors());
+const client = new pg.Client(process.env.DATABASE_URL);
 
 // Global Variables
 const PORT = process.env.PORT || 3000;
@@ -20,16 +21,25 @@ app.get('/todo', handleToDo);
 // Route Handlers
 function handleToDo(request, response) {
 
-  let thingsToDo = [
-    { task: 'watch tv' },
-    { task: 'walk rosie' },
-    { task: 'practice for lecture' },
-    { task: 'cooking' },
-    { task: 'eat cookies' },
-    { task: 'take a nap' },
-  ];
+  const SQL = 'SELECT * FROM tasks;';
+  client.query(SQL)
+    .then(results => {
+      console.log(results);
+      response.status(200).json(results.rows);
+    })
+    .catch(response.status(500).send('Something went wrong fetching from database'));
 
-  response.status(200).json(thingsToDo);
+
+  // let thingsToDo = [
+  //   { task: 'watch tv' },
+  //   { task: 'walk rosie' },
+  //   { task: 'practice for lecture' },
+  //   { task: 'cooking' },
+  //   { task: 'eat cookies' },
+  //   { task: 'take a nap' },
+  // ];
+
+  // response.status(200).json(thingsToDo);
 }
 
 // Go!
